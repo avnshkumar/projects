c42cca652dbd02afefd5ed397f20d63ff4ffce18
Avinash Kumar <avinash@strings.ai>
2025-06-13 00:21:17 +0530

    added endpoint to interact via scanner

--- java-apps/parking-lot/src/main/java/com/example/runners/ParkingShellRunner.java
+++ java-apps/parking-lot/src/main/java/com/example/runners/ParkingShellRunner.java
@@ -5,38 +5,37 @@ import com.example.components.commands.CommandExecutor;
 import com.example.components.commands.CommandExecutorFactory;
 import com.example.constants.CommandName;
 import com.example.services.ParkingService;
+import org.springframework.stereotype.Service;
 
 import java.util.Scanner;
 
-public class ParkingShellRunner implements Runnable {
-
-
-
-    public static void main(String[] args) {
-        ParkingShellRunner runner = new ParkingShellRunner();
-        Thread thread = new Thread(runner);
-        thread.start();
-    }
-
-    @Override
-    public void run(){
+@Service
+public class ParkingShellRunner {
+    private Runnable runnable = () -> {
         ParkingService parkingService = new ParkingService(null);
         CommandExecutorFactory commandExecutorFactory = new CommandExecutorFactory(parkingService);
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Starting on thread: " + Thread.currentThread().getName() + "\n");
         System.out.println("Welcome to the Parking Lot Management System!");
-        while(true){
+        while (true) {
+            System.out.print("Running on thread: " + Thread.currentThread().getName() + "\n");
             String input = scanner.nextLine().trim();
             Command command = Command.parse(input);
             CommandExecutor commandExecutor = commandExecutorFactory.getCommandExecutor(command.getName());
             try {
                 commandExecutor.execute(command);
-                if (command.getName().equals(CommandName.EXIT)){
+                if (command.getName().equals(CommandName.EXIT)) {
                     return;
                 }
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
+    };
+
+    public void start() {
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
 }
